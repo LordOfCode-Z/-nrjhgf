dd236b61fc25da9b9d737c12fe9fc5dcd0e416f3
LordOfCode-Z <117388710+LordOfCode-Z@users.noreply.github.com>
2023-01-31 22:21:05 +0300

    Update PORT TARAMA.py

--- PORT TARAMA.py
+++ PORT TARAMA.py
@@ -12,15 +12,15 @@ Port tarama aracına hoş geldin :)
 3) İşletim Sistemi Bilgisi
 """)
 
-islemno = raw_input("İşlem Numarasını Giriniz: ")
+islemno = input("İşlem Numarasını Giriniz: ")
 
 if(islemno=="1"):
-    hedefip = raw_input("Hedef Ip Giriniz: ")
+    hedefip = input("Hedef Ip Giriniz: ")
     os.system("nmap"+ hedefip)
 elif(islemno=="2"):
-    hedefip = raw_input("Hedef Ip Giriniz: ")
+    hedefip = input("Hedef Ip Giriniz: ")
     os.system("nmap -sS -sV "+ hedefip)
 elif(islemno=="3"):
-    hedefip = raw_input("Hedef Ip Giriniz: ")
+    hedefip = input("Hedef Ip Giriniz: ")
     os.system("nmap -0 "+ hedefip)
 else: print("Hatalı Seçim Yaptınız Bay xxx")
